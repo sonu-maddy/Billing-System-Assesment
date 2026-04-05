f2c6d341a31913034ec0480f35f258d72a8cd670
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2026-04-05 21:45:01 +0530

    final working code

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,11 @@ import invoiceRoutes from "./routes/invoiceRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(express.json());
 
 app.use("/customers", customerRoutes);
